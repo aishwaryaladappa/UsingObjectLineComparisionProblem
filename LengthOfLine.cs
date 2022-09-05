d6a717b8587b8a4717a11f3777a8a3a4f03788ee
aishwaryaladappa <aishwaryaladapppa971@gmail.com>
2022-09-05 16:34:14 +0530

    LengthOfLine.cs

---
+++ LengthOfLine.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LineComparisionProblem
+{
+    public class LengthOfLine
+    {
+
+
+
+        public void CalculateLengthOfLine()
+        {
+            double x1 = 5, x2 = 4, y1 = 5, y2 = 3;
+            double Length;
+            Console.WriteLine("enter x1,y1,x2,y2:");
+
+            Length = Math.Sqrt((x2 - x1) * (x2 - x1) + (y2 - y1) * (y2 - y1));
+            Console.WriteLine("LengthOfLine" + Length);
+
+        }
+    }
+}
