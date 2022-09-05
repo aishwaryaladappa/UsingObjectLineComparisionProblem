712d85c3cf52158a6a29e03aadaf0be180dc985b
aishwaryaladappa <aishwaryaladapppa971@gmail.com>
2022-09-05 16:32:17 +0530

    added Program.cs

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+namespace LineComparisionProblem
+{
+    class program
+    {
+        public static void Main(string[] args)
+        {
+            LengthOfLine lengthOfLine = new LengthOfLine();
+            lengthOfLine.CalculateLengthOfLine();
+            CompareLengthOfTwoLine compareLengthOfTwoLine = new CompareLengthOfTwoLine();
+            compareLengthOfTwoLine.CheckEqualityOfLines();
+            LineEquality lineEquality = new LineEquality();
+            lineEquality.CheckEqualityOfLines();
+
+
+        }
+
+    }
+}
+
