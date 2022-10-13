f71bdbae3539edf188847d53dcf72fd0f9e840fd
aishwaryaladappa <aishwaryaladapppa971@gmail.com>
2022-10-13 17:30:04 +0530

    added program.cs

--- Program.cs
+++ Program.cs
@@ -5,12 +5,13 @@ namespace LineComparisionProblem
     {
         public static void Main(string[] args)
         {
+            Console.WriteLine("Welcome to line comparision problem");
             LengthOfLine lengthOfLine = new LengthOfLine();
             lengthOfLine.CalculateLengthOfLine();
-            CompareLengthOfTwoLine compareLengthOfTwoLine = new CompareLengthOfTwoLine();
-            compareLengthOfTwoLine.CheckEqualityOfLines();
-            LineEquality lineEquality = new LineEquality();
-            lineEquality.CheckEqualityOfLines();
+            //CompareLengthOfTwoLine compareLengthOfTwoLine = new CompareLengthOfTwoLine();
+            //compareLengthOfTwoLine.CheckEqualityOfLines();
+            //LineEquality lineEquality = new LineEquality();
+            //lineEquality.CheckEqualityOfLines();
 
 
         }
